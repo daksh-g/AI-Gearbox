2eccc2443b4040a980771a0843373778afaabfa0
Abhay <80220229+EpicGamer007@users.noreply.github.com>
2023-12-31 16:48:48 -0800

    Start book search stuff

--- src/public/scripts/booksearch.js
+++ src/public/scripts/booksearch.js
@@ -1,7 +1,29 @@
 const titleElem = document.querySelector(".search input");
 const button = document.querySelector(".search img");
+const resultsElement = document.querySelector("#results");
 
 button.addEventListener("click", async () => {
     const response = await (await fetch(`/api/books?bookname=${titleElem.value}`)).json();
-    console.log(response);
+    
+    response.results.forEach((result) => {
+        const bookData = {
+            authors: result.authors[0], // A string with the author of the book
+            genres: result.categories[0], // A string with all the genres in the book
+            pagesCount: result.page_count,
+            title: result.title,
+            summary: result.summary
+        };
+
+        console.log(bookData);
+
+        // TODO: Add css for this book
+        const book = document.createElement("div");
+        book.classList.add("book");
+        
+        // Make elements for each of the book data fields and add them to the book element
+
+        resultsElement.appendChild(book);
+
+    });
+
 });
